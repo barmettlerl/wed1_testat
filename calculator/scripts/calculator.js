fcedbe996f3a3324e98375bcf75040b50b14e14b
theMaimu <barmettler.lars@gmail.com>
2018-05-16 14:24:22 +0200

    sepearate calculator into Logic and UI

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -1,127 +1,12 @@
 /**
  * core
  */
-let calculator = {
-    numberButtons: null,
-    operatorButtons: null,
-    commandButtons: null,
-    input: null,
-    output: null,
-    operator: "",
-    term1: "",
-    term2: "",
-
-    setTerm: function(value){
-        this.term1 += value;
-        this.input.value = this.term1;
-    },
-
-    setOperator: function (elem) {
-
-        if(this.operator === ''){
-            this.operator = elem.value;
-            this.output.value = this.term1 + " " + elem.value;
-            this.term2 = this.term1;
-        }else {
-            this.operator = elem.value;
-            this.output.value = this.output.value.substr(0, this.output.value.length - 1) + this.operator;
-        }
-        this.input.value = "";
-        this.term1 = "";
-    },
-
-    clear: function () {
-        this.operator = '';
-        this.term1 = "";
-        this.input.value = "";
-        this.term2 = "";
-        this.output.value = "";
-    },
-
-    init: function () {
-
-        this.numberButtons = document.querySelectorAll('.number');
-        this.operatorButtons = document.querySelectorAll('.operator');
-        this.commandButtons = document.querySelectorAll('.command');
-        this.input = document.querySelector('.input');
-        this.output = document.querySelector('.output');
-        this.input = document.querySelector('.input');
-
-        // Setze Welcome Nachricht
-        this.initWelcomeMessage();
-
-        // Instantiate Event Listener
-        this.hideWelcomeMessage();
-        this.instantiateEventsForTerm(this.numberButtons);
-        this.instantiateEventForOperator(this.operatorButtons);
-        this.instantiateEventForCommands(this.commandButtons);
-    },
-
-    /**
-     * Stelle die Welcome-Nachricht initial dar
-     */
-    initWelcomeMessage: function() {
-        document.querySelector("#output").value = "Welcome";
-    },
-
-    /**
-     * Blende die Welcome-Nachricht aus
-     */
-    hideWelcomeMessage: function() {
-        document.querySelectorAll("form button").forEach(function(elem) {
-            elem.addEventListener("click", function () {
-                if(document.querySelector("#output").value === "Welcome") {
-                    document.querySelector("#output").value = "";
-                }
-            });
-        });
-    },
-
-    /**
-     * Blabla
-     * @param elements
-     */
-    instantiateEventsForTerm: function (elements) {
-        let self = this;
-
-        elements.forEach((elem) => {
-            elem.addEventListener('click', function (event) {
-                self.setTerm(parseFloat(event.target.value));
-            })
-        })
-    },
-
-    instantiateEventForOperator: function (elements) {
-        let self = this;
-
-        elements.forEach((elem) => {
-            elem.addEventListener('click', function (event) {
-                self.setOperator(event.target);
-            })
-        })
-    },
-
-    instantiateEventForCommands: function (elements) {
-        let self = this;
-
-        elements.forEach((elem) => {
-            elem.addEventListener('click', function (event) {
-                console.log(event.target.innerHTML);
-                if(event.target.innerHTML === 'C'){
-                    self.clear();
-                }else if(event.target.innerHTML === '=' ){
-                    self.calculate(self.term1, self.term2, self.operator);
-                }
-            })
-        })
-    },
-
-    calculate: function (term1, term2, operator) {
+const calculator = {
+  calc: function (term1, term2, operator) {
         let solution = 0;
 
         if(term2 === ''){
-            this.output.value = 'Invalid calculation';
-            return;
+            return 'Invalid calculation';
         }
 
         term1 = parseFloat(term1);
@@ -139,15 +24,13 @@ let calculator = {
                 break;
             case '/':
                 if(term1 === 0){
-                    this.output.value = "You've destroyed the universe, thx";
-                    return;
+                    return "You've destroyed the universe, thx";
                 }else{
                     solution = term2 / term1;
                 }
         }
 
-        this.clear();
-        this.setTerm(solution);
+        return solution;
     }
 
 }
@@ -156,6 +39,125 @@ let calculator = {
  * UI
  */
 
+const calculatorUI = {
+  calculator : null,
+  numberButtons: null,
+  operatorButtons: null,
+  commandButtons: null,
+  input: null,
+  output: null,
+  term1: "",
+  term2: "",
+  operator: "",
+
+  init: function (calculator) {
+    this.calculator = calculator
+    this.numberButtons = document.querySelectorAll('.number');
+    this.operatorButtons = document.querySelectorAll('.operator');
+    this.commandButtons = document.querySelectorAll('.command');
+    this.input = document.querySelector('.input');
+    this.output = document.querySelector('.output');
+    this.input = document.querySelector('.input');
+
+    // Setze Welcome Nachricht
+    this.initWelcomeMessage();
+
+    // Instantiate Event Listener
+    this.hideWelcomeMessage();
+    this.instantiateEventsForTerm(this.numberButtons);
+    this.instantiateEventForOperator(this.operatorButtons);
+    this.instantiateEventForCommands(this.commandButtons);
+  },
+
+  /**
+   * Stelle die Welcome-Nachricht initial dar
+   */
+  initWelcomeMessage: function() {
+    document.querySelector("#output").value = "Welcome";
+  },
+
+  /**
+   * Blende die Welcome-Nachricht aus
+   */
+  hideWelcomeMessage: function() {
+    const self = this;
+    document.querySelectorAll("form button").forEach(function(elem) {
+      elem.addEventListener("click", function () {
+        if(self.output.value === "Welcome") {
+          self.output.value = "";
+        }
+      });
+    });
+  },
+
+  /**
+   * Initiate the Events
+   * @param elements
+   */
+  instantiateEventsForTerm: function (elements) {
+    let self = this;
+
+    elements.forEach((elem) => {
+      elem.addEventListener('click', function (event) {
+        self.setTerm(parseFloat(event.target.value));
+      })
+    })
+  },
+
+  instantiateEventForOperator: function (elements) {
+    let self = this;
+
+    elements.forEach((elem) => {
+      elem.addEventListener('click', function (event) {
+        self.setOperator(event.target);
+      })
+    })
+  },
+
+  instantiateEventForCommands: function (elements) {
+    let self = this;
+
+    elements.forEach((elem) => {
+      elem.addEventListener('click', function (event) {
+        if(event.target.innerHTML === 'C'){
+          self.clear();
+        }else if(event.target.innerHTML === '=' ){
+          const result = calculator.calc(self.term1, self.term2, self.operator);
+          self.clear();
+          self.setTerm(result);
+        }
+      })
+    })
+  },
+
+  setTerm: function(value){
+    this.term1 += value;
+    this.input.value = this.term1;
+  },
+
+  setOperator: function (elem) {
+    if(this.operator === ''){
+      this.operator = elem.value;
+      this.output.value = this.term1 + " " + elem.value;
+      this.term2 = this.term1;
+    }else {
+      this.operator = elem.value;
+      this.output.value = this.output.value.substr(0, this.output.value.length - 1) + this.operator;
+    }
+
+    this.input.value = "";
+    this.term1 = "";
+  },
+
+  clear: function () {
+    this.output.value = "";
+    this.input.value = "";
+    this.term1 = "";
+    this.term2 = "";
+    this.operator = "";
+  }
+}
+
 window.addEventListener('load', function () {
-    calculator.init();
+    calculatorUI.init(calculator);
 });
